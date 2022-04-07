5163a08e5f92d24aad9a0bc5cb6102840f1156dc
palomovisk@gmail.com <lucas.serna.quinto@gmail.com>
2022-04-06 23:37:48 -0300

    perguntando por coordenadas

--- ui/pages/maps.tsx
+++ ui/pages/maps.tsx
@@ -4,18 +4,43 @@ import Layout from "../components/layout";
 import Map from "../components/map";
 import Marker from "../components/marker";
 
+interface GeoPosition {
+  lat: number,
+  lng: number
+}
+var pst : GeoPosition = {
+  lat:-23.06,
+  lng:-46.5
+};
+
 const render = (status: Status) => {
   return <h1>{status}</h1>;
 };
 
+
+
+
 export default function Maps() {
+  let location: any ;
+  if (navigator.geolocation) {
+    location = navigator.geolocation.getCurrentPosition(getPosition);
+  }
+
   const ref = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState<google.maps.Map>();
   const [zoom, setZoom] = useState(10);
-  const [center, setCenter] = useState<google.maps.LatLngLiteral>({
-    lat: -23.06,
-    lng: -46.5,
-  });
+  const [center, setCenter] = useState<google.maps.LatLngLiteral>(pst);
+
+  
+  
+  function getPosition(position:GeolocationPosition):GeoPosition {
+    console.log(position.coords.latitude, position.coords.longitude);
+    pst = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    }
+    return pst;
+  }
 
   useEffect(() => {
     if (ref.current && !map) {
