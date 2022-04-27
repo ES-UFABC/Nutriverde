4ae418a10f2e5dd105cae6e5732c08dc214c9b4f
Breno Boato da Silva <breno.s@aluno.ufabc.edu.br>
2022-04-27 00:57:35 -0300

    feat(ui): Finaliza visual informações do produto.
    Co-authored-by: Jonathan Takeshi Ywahashi <jonathan.ywahashi@gmail.com>

--- ui/pages/products/[id].tsx
+++ ui/pages/products/[id].tsx
@@ -23,92 +23,75 @@ export default function ProductAbout() {
     <Layout title={`Product - ${product?.name}`}>
       {/* <img src={`${serverUrl}/files/${product?.cover}`}></img> */}
       <div className=" mx-auto p-4 flex flex-col ">
-        <div className="flex flex-row p-4 w-full text-center bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <div className="flex flex-row gap-2 p-4 w-full text-center bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
           <div className="flex flex-row basis-2/5 ">
             <div className="flex flex-col basis-1/4 gap-2 ">
               <img src="/home1.png"></img>
               <img src="/home2.png"></img>
               <img src="/home3.png"></img>
             </div>
-             <div className=" flex basis-3/4 ml-2 place-content-center flex-none  ">
-            <img className="" src="/home1.png"></img>
+            <div className=" flex basis-3/4 ml-2 place-content-center flex-none  ">
+              <img className="" src="/home1.png"></img>
             </div>
           </div>
 
-          <div className="basis-3/5">
-            <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">
-              Work fast from anywhere
-            </h5>
-            <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
-              Stay up to date and move work forward with Flowbite on iOS &
-              Android. Download the app today.
+          <div className="basis-3/5 flex flex-col gap-6">
+            <p className="text-4xl font-bold text-center my-4">
+              Sopa de cogumelo e batata e cenoura
             </p>
 
-            <div className="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-              <a
-                href="#"
-                className="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
-              >
-                <svg
-                  className="mr-3 w-7 h-7"
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fab"
-                  data-icon="apple"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 384 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"
-                  ></path>
-                </svg>
-                <div className="text-left">
-                  <div className="mb-1 text-xs">Download on the</div>
-                  <div className="-mt-1 font-sans text-sm font-semibold">
-                    Mac App Store
-                  </div>
+            <div className="flex flex-row">
+              <div className="flex flex-col basis-1/2 items-start ml-28 gap-3  ">
+                <p className="text-xl">
+                  <span className="font-bold">Preço:</span>R$25,00
+                </p>
+                <p className="text-xl">
+                  <span className="font-bold">Tipo:</span>Tubérculo
+                </p>
+                <p className="text-xl">
+                  <span className="font-bold">Safra:</span>11/10/2001
+                </p>
+                <p className="text-xl">
+                  <span className="font-bold">Produtor:</span>
+                  <a href="/producers/1">Carlos Augusto</a>
+                </p>
+                <div>
+                  <p className="font-bold text-xl text-left">Descrição:</p>
+                  <p className="text-justify">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </p>
                 </div>
-              </a>
-              <a
-                href="#"
-                className="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
-              >
-                <svg
-                  className="mr-3 w-7 h-7"
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fab"
-                  data-icon="google-play"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"
-                  ></path>
-                </svg>
-                <div className="text-left">
-                  <div className="mb-1 text-xs">Get in on</div>
-                  <div className="-mt-1 font-sans text-sm font-semibold">
-                    Google Play
-                  </div>
+              </div>
+              <div className="flex flex-col items-center basis-1/2">
+                <div className="relative z-0 mb-6 w-32  group">
+                  <input
+                    type="number"
+                    name="quantity"
+                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-emerald-800 peer"
+                    placeholder=" "
+                  />
+                  <label
+                    htmlFor="quantity"
+                    className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-emerald-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                  >
+                    Quantidade
+                  </label>
                 </div>
-              </a>
+                <button className="btn mb-3 mr-2 w-60 mt-3">
+                  Adicionar ao carrinho
+                </button>
+
+                <p>Em estoque:85</p>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800> dark:border-gray-700 dark:hover:bg-gray-700">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
         </div>
       </div>
     </Layout>
