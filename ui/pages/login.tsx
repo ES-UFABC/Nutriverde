903e7f75aefb9d0852eced63db173410fbfd5709
vitorsniquer <vitorsniquer@gmail.com>
2022-04-27 22:02:52 -0300

    fix(ui) change login form colors

--- ui/pages/login.tsx
+++ ui/pages/login.tsx
@@ -62,8 +62,8 @@ export default function Component(props: any) {
         pauseOnHover
       />
       <div className="flex justify-center">
-        <div className="w-full max-w-xs ">
-          <form action="/login" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 " onSubmit={submit}>
+        <div className="w-full max-w-xs  ">
+          <form action="/login" className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 mb-4 " onSubmit={submit}>
             <div className="mb-4 ">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                 Email
@@ -74,10 +74,10 @@ export default function Component(props: any) {
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                 Password
               </label>
-              <input className="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="***************" name="password" />
+              <input className="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******" name="password" />
             </div>
             <div className="flex items-center justify-between">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+              <button className="bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                 Sign In
               </button>
             </div>
