befadcc31c014a7b9fbd58d506ce5e692c3cde5a
Breno Boato da Silva <breno.s@aluno.ufabc.edu.br>
2022-05-09 23:55:50 -0300

    wip(ui): Insere review
    Co-authored-by: Jonathan Takeshi Ywahashi <jonathan.ywahashi@gmail.com>

--- ui/pages/products/[id].tsx
+++ ui/pages/products/[id].tsx
@@ -8,12 +8,15 @@ import {
 } from "@heroicons/react/outline";
 import classNames from "classnames";
 import { format } from "date-fns";
+import { Formik } from "formik";
 import { GetServerSidePropsContext } from "next";
 import Link from "next/link";
 import { useState } from "react";
 import NumberFormat from "react-number-format";
+import InputField from "../../components/form/input-field";
 import Layout from "../../components/layout";
 import { IProducer, IProduct } from "../../interfaces";
+import * as Yup from "yup";
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
@@ -245,76 +248,115 @@ export default function ProductAbout({
                 className="divide-y divide-gray-200 dark:divide-gray-700"
               >
                 <li className="py-3 sm:py-4">
-                  <div className="flex items-center space-x-4">
-                    <div className="flex-shrink-0 content-center">
-                      <UserCircleIcon className="block w-8 h-8" />
-                      <p className="text-xs">Julinho</p>
-                    </div>
-                    <div className="flex-1 min-w-0">
-                      <div className="">
-                        <input
+                  <Formik
+                    initialValues={{ title: "", content: "", rating: 0 }}
+                    validationSchema={Yup.object().shape({
+                      title: Yup.string().required("Obrigatório"),
+                      content: Yup.string().required("Obrigatório"),
+                      rating: Yup.number().integer().min(1).max(5),
+                    })}
+                    onSubmit={(values) => console.log(values)}
+                  >
+                   
+                    {({
+                      values,
+                      isValid,
+                      isSubmitting,
+                      errors,
+                      setFieldValue,
+                      setFieldTouched,
+                      handleSubmit,
+                      validateField,
+                    }) => (
+                      <form onSubmit={handleSubmit}>
+                        <div className="flex items-center space-x-4">
+                          <div className="flex-shrink-0 content-center">
+                            <UserCircleIcon className="block w-8 h-8" />
+                            <p className="text-xs">Julinho</p>
+                          </div>
+                          <div className="flex-1 min-w-0 text-left">
+                            <div className="">
+                              {/* <input
                           className="text-xl w-full"
                           placeholder="Título"
-                        />
-                      </div>
-                      <div>
-                        <textarea
-                          className="text-md w-full"
-                          placeholder="Escreva seu comentário"
-                        />
-                      </div>
-                      <div className="flex flex-row">
-                        <p className="text-xs text-left basis-1/2">12/05/2005</p>
-                        <div className=" flex basis-1/2 place-content-end">
-                        <button className=" btn btn-primary btn-sm text-xs ml-3  "><PaperAirplaneIcon className="block w-4 h-4"/>SUBMETER</button>
+                        /> */}
+                              <InputField name="title"
+                                className="text-xl w-full"
+                                type="text"
+                                label="Título"
+                              />
+                            </div>
+                            <div>
+                            <InputField name="content"
+                               className="text-md w-full"
+                                as = "textarea"
+                                label="Review"
+                              />
+                              {/* <textarea
+                                className="text-md w-full"
+                                placeholder="Escreva seu comentário"
+                              /> */}
+                            </div>
+                            <div className="flex flex-row">
+                              <p className="text-xs text-left basis-1/2">
+                                12/05/2005
+                              </p>
+                              <div className=" flex basis-1/2 place-content-end">
+                                <button className=" btn btn-primary btn-sm text-xs ml-3  ">
+                                  <PaperAirplaneIcon className="block w-4 h-4" />
+                                  SUBMETER
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                            <div className="flex items-center">
+                              <svg
+                                className="w-5 h-5 text-yellow-400"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                              </svg>
+                              <svg
+                                className="w-5 h-5 text-yellow-400"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                              </svg>
+                              <svg
+                                className="w-5 h-5 text-yellow-400"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                              </svg>
+                              <svg
+                                className="w-5 h-5 text-yellow-400"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                              </svg>
+                              <svg
+                                className="w-5 h-5 text-gray-300 dark:text-gray-500"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                              </svg>
+                            </div>
+                          </div>
                         </div>
-                      </div>
-                    </div>
-                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                      <div className="flex items-center">
-                        <svg
-                          className="w-5 h-5 text-yellow-400"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                        </svg>
-                        <svg
-                          className="w-5 h-5 text-yellow-400"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                        </svg>
-                        <svg
-                          className="w-5 h-5 text-yellow-400"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                        </svg>
-                        <svg
-                          className="w-5 h-5 text-yellow-400"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                        </svg>
-                        <svg
-                          className="w-5 h-5 text-gray-300 dark:text-gray-500"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                        </svg>
-                      </div>
-                    </div>
-                  </div>
+                      </form>
+                    )}
+                  </Formik>
                 </li>
                 <li className="py-3 sm:py-4">
                   <div className="flex items-center space-x-4">
